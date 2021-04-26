3f31025513ce0dd6e2bb2d531cb86c2848874fbf
shiyamu <shiyamu.dss@gmail.com>
2021-04-26 15:42:12 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -16,7 +16,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150,300,300,180);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -48,6 +49,8 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
+
+    platform.display();
 
     box3.display();
     box4.display();
